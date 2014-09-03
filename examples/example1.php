c3a3e36dcea2da36b9b96444a281a5104a871ea1
Wallace C Ricardo <wallace.crd@gmail.com>
2014-09-02 21:15:36 -0400

    Update example1.php

--- examples/example1.php
+++ examples/example1.php
@@ -1,4 +1,4 @@
 <?php
     require(__DIR__."/../log.php");
-    Uberlog\Log::info("category_x", "something_happened"
-    Uberlog\Log::success("category_x", "something_else_happened",array("user" => "some user"));
+    UberLog\Log::info("category_x", "something_happened"
+    UberLog\Log::success("category_x", "something_else_happened",array("user" => "some user"));
